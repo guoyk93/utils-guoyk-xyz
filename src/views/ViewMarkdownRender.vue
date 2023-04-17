16b4b3d40960e7144466401ad7a5fcc89694b358
Guo Y.K <hi@guoyk.net>
2023-04-17 11:17:47 +0800

    update

--- src/views/ViewMarkdownRender.vue
+++ src/views/ViewMarkdownRender.vue
@@ -48,24 +48,47 @@ div#main-content {
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
 
+div#main-content blockquote {
+    padding-left: 0.6rem;
+    border-left: 4px solid whitesmoke;
+}
+
 div#main-content h1 {
+    text-align: center;
     font-size: 2.0rem;
+    margin-top: 1.4rem;
+    margin-bottom: 1.4rem;
 }
 
 div#main-content h2 {
     font-size: 1.8rem;
+    margin-top: 1.2rem;
+    margin-bottom: 1.2rem;
 }
 
 div#main-content h3 {
     font-size: 1.6rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
 }
 
 div#main-content h4 {
+    margin-top: 0.8rem;
     font-size: 1.4rem;
 }
 
 div#main-content h5 {
     font-size: 1.2rem;
+}
+
+div#main-content img {
+    max-width: 90%;
+    max-height: 32rem;
+    padding: 0.4rem;
+}
+
+div#main-content pre code {
+    font-size: 0.8rem;
 }
 
 </style>
